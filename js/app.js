0276ae877f6e1a8b2e3bf35a37b6a56465dd2d39
OwethuSotomela <owethusotomela@gmail.com>
2022-05-09 09:08:35 +0200

    Automating data

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 document.addEventListener('alpine:init', ()=>{
-    Alpine.data('data', ()=>{
+    Alpine.data('small', ()=>{
         return {
             init() {
-                console.log('Run when the page load');
                 console.log(this.orders[0].subTotal())
             },
             open: false,
             orders: [
                 {
-                    size: 'small',
+                    size: 'Small',
                     name: 'Small Pizaa',
                     price: 48.99,
-                    qty: 2,
-                    description: 'Wowow',
+                    qty: 1,
+                    description: 'Small pizza with 3 toppings. 1 topping. 3 or less other toppings.',
                     subTotal() {
                         return Number(this.price) * Number(this.qty)
                     },
@@ -28,3 +27,60 @@ document.addEventListener('alpine:init', ()=>{
     })
 })
 
+document.addEventListener('alpine:init', ()=>{
+    Alpine.data('medium', ()=>{
+        return {
+            init() {
+                console.log(this.orders[0].subTotal())
+            },
+            open: false,
+            orders: [
+                {
+                    size: 'Medium',
+                    name: 'Medium Pizza',
+                    price: 78.99,
+                    qty: 1,
+                    description: 'Medium margerita pizza with 3 toppings max. 2 oe less meat topping. 3 or less other toppings',
+                    subTotal() {
+                        return Number(this.price) * Number(this.qty)
+                    },
+                    add() {
+                        this.qty++
+                    }
+                }, 
+            ],
+            getOrder(){
+
+            }
+        }
+    })
+})
+
+document.addEventListener('alpine:init', ()=>{
+    Alpine.data('large', ()=>{
+        return {
+            init() {
+                console.log(this.orders[0].subTotal())
+            },
+            open: false,
+            orders: [
+                {
+                    size: 'Large',
+                    name: 'Large Pizza',
+                    price: 114.99,
+                    qty: 1,
+                    description: 'Large margerita pizza with 3 toppings max. 3 meat toppingds max. 3 or less other toppings.',
+                    subTotal() {
+                        return Number(this.price) * Number(this.qty)
+                    },
+                    add() {
+                        this.qty++
+                    }
+                }, 
+            ],
+            getOrder(){
+
+            }
+        }
+    })
+})
